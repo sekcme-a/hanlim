13dcca170f0bb86986ae32dc918cb59a448569ea
sekcme-a <sekcme21s@naver.com>
2021-12-29 13:32:56 +0900

    asdf

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -9,12 +9,12 @@ const Footer = () => {
       <div className="footer-left">
         <h3 className="footer-logo">벤처한림회</h3>
         <div className="footer-info">
-          <p>상호 : (사)벤처한림회 | 대표자명 : 김균식</p>
+          <p>상호 : (사)벤처한림회 | 대표자명 : 000</p>
           <p>고유번호 : 000-00-00000</p>
           <p>대표번호 : 00-0000-0000 | 팩스 : 00-0000-0000 | 이메일 : hanlim@naver.com</p>
           <p>주소 : 본부 - 장안평 사무실(서울 동대문구 장안동 465-11, 세우빌딩)<br />서울지사 - 율곡로 사무실(서울 종로구 율곡로 160)</p>
         </div>
-        <p className="footer-company-name">대한생활체육회 &copy; 2021</p>
+        <p className="footer-company-name">벤처한림회 &copy; 2021</p>
       </div>
       <div className="footer-center">
         <div>
